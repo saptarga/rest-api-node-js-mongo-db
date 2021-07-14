ff299bc80f2ddd8167ee831576c227cacd827606
saptarga <saptarga@gmail.com>
2021-07-14 11:45:20 +0700

    Add morgan for basc logger

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
 require('dotenv/config')
 require('./config/connection')
 const app = express()
 
 app.use(express.json());
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 
 // Import Post Routes 
 const postsRoute = require('./routes/posts.route')

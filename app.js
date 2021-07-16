acf4afbf51ece591e9d07e228fb1ed6fc58e3b4b
saptarga <saptarga@gmail.com>
2021-07-16 10:30:51 +0700

    Add basic auth

--- app.js
+++ app.js
@@ -2,6 +2,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const basicAuth = require("express-basic-auth");
+
+var staticUserAuth = basicAuth({
+	users: {
+		Admin: "secret1234",
+	},
+	challenge: false,
+});
 
 // include config and connection db
 require("dotenv/config");
@@ -15,12 +23,30 @@ app.use(
 	morgan(":method :url :status :res[content-length] - :response-time ms")
 );
 
+/**
+ * add simple auth with basic auth
+ * * please comment if you don't want to use auth
+ * * if you want to Custom authorization, you can check in npm express-basic-auth documentation
+ * TODO: change response with json response
+ * */
+app.use(
+	basicAuth({
+		users: {
+			Admin: "secret1234",
+		},
+		challenge: false,
+		unauthorizedResponse: (req) => {
+			return "Unauthorized user";
+		},
+	})
+);
+
 // Import Post Routes
 const postsRoute = require("./routes/posts.route");
 app.use("/posts", postsRoute);
 
 //Default Route
-app.get("/", (req, res) => {
+app.get("/", staticUserAuth, (req, res) => {
 	res.send("Home");
 });
 
